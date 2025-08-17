37b02d098b30e157c2616dc88c3f3767a3468ee0
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-18 01:39:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 // Complete the js code
 function Car(make, model) {
-  constructor(make,model){
+ 
 	  this.make=make;
 	  this.model=model;
-  }
-	getMakeModel(){
-		return '${this.make} ${this.model}';
+
+ Car.prototype.getMakeModel=function(){
+		return `${this.make} ${this.model}`;
 	}
 	
 }
 
 function SportsCar(make, model, topSpeed) {
-	constructor(make,model,topSpeed){
-		super(make,model);
-		this.topSpeed=topSpeed;
-	}
 
-	getTopSpeed(){
-		return this.topSpeed;
-	}
+		Car.call(this,make,model);
+		this.topSpeed=topSpeed;
+	
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+    return `${this.topSpeed}`;
 }
 
 // Do not change the code below
